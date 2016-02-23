10635b6fc6e59792d995e17739271102bc8f18b0
shravin <shravin@gmail.com>
2016-02-23 18:18:50 +0530

    added the question
    Change-Id: I170d6fbe4b3a2d0c73d371b4c27bf135eb07cc23

--- HackerRank/src/com/hackerrank/AmazonQ1.java
+++ HackerRank/src/com/hackerrank/AmazonQ1.java
@@ -1,3 +1,12 @@
+/*
+Given an array of 0s and 1s, and k, Find the longest continuous streak of 1s after flipping k 0s to 1s.
+
+E.x array is {1,1,0,0,1,1,1,0,1,1}
+k = 1 (which means we can flip ‘k’ one 0 to 1)
+
+Answer: 6 (if we flip 0 at index 7, we get the longest continuous streak of 1s having length 6)
+*/
+
 package com.hackerrank;
 
 public class AmazonQ1 {
