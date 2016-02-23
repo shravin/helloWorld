cc11d544b59df2fefb7c17ff7a2033a39e102ef8
shravin <shravin@gmail.com>
2016-02-23 13:03:53 +0530

    Working copy
    Change-Id: I87d1819619d6629dbf2f5c38bac386ece00e66ae

--- HackerRank/src/com/hackerrank/Solution.java
+++ HackerRank/src/com/hackerrank/Solution.java
@@ -1,13 +1,9 @@
 package com.hackerrank;
-import java.io.*;
-import java.util.*;
 
 public class Solution {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		System.out.println(scanner.next());
+		System.out.println("Hello World!!!");
 	}
 
 }
